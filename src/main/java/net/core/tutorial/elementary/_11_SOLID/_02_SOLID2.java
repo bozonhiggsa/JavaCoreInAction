2306a91149e013277161373c5f0cc4b49c5b06f3
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2019-11-21 09:21:21 +0200

    add examples

---
+++ src/main/java/net/core/tutorial/elementary/_11_SOLID/_02_SOLID2.java
@@ -0,0 +1,12 @@
+package net.core.tutorial.elementary._11_SOLID;
+
+/**
+
+ Open-Closed Principle.
+ Программные сущности (классы, модули, функции) должны быть открыты для расширения, но не для модификации.
+
+ 
+
+ */
+public class _02_SOLID2 {
+}
