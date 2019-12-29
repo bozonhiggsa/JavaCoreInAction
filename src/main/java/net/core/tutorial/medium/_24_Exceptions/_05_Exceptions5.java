31c33c967f6b4080399db951c85d5ac203804925
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2019-12-29 19:18:22 +0200

    add examples

--- src/main/java/net/core/tutorial/medium/_24_Exceptions/_05_Exceptions5.java
+++ src/main/java/net/core/tutorial/medium/_24_Exceptions/_05_Exceptions5.java
@@ -1,10 +1,14 @@
 package net.core.tutorial.medium._24_Exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
 
  Если метод должен возвращать результат, но в нём внутри есть создание исключения (не в секции try),
  которое пробрасывается дальше в сигнатуре метода, то он не может иметь return в конце.
  После кидания исключения до строки с return даже не дойдёт, поэтому её и не должно быть.
+
  Нельзя использовать return и в случае, когда исключение кидается в секции try, но нет секции catch (а есть finally),
  и идёт проброс сразу в сигнатуру метода.
 
@@ -40,6 +44,97 @@ public class _05_Exceptions5 {
 
     public static void main(String[] args) {
 
+        try{
+            SomeClass1.someMethod1();
+        }
+        catch (Exception e){
+            System.out.println("Метод выбросил исключение: " + e.getMessage());
+        }
 
+        try{
+            SomeClass1.someMethod2();
+        }
+        catch (Exception e){
+            System.out.println("Метод выбросил исключение: " + e.getMessage());
+        }
+
+        System.out.println("-----------------------");
+
+        SomeClass1.someMethod3();
+
+        System.out.println("-----------------------");
+
+        System.out.println(SomeClass1.someMethod4());
+    }
+}
+
+class SomeClass1 {
+
+    public static int someMethod1() throws Exception {
+
+        throw new Exception("Исключение типа Exception");
+    }
+
+    public static String someMethod2() throws IOException {
+
+        try{
+            throw new IOException("Исключение IOException");
+        }
+        finally {
+            System.out.println("Отработала секция finally");
+        }
+    }
+
+    public static void someMethod3() {
+
+        int number = (int) ((Math.random() - 0.5) * 100);
+
+        try{
+            if(number > 0){
+                System.out.println("Выполнилось условие в секции try");
+                System.out.println("Секция try отработала без исключения");
+                return;
+            }
+            throw new FileNotFoundException("Исключение FileNotFoundException");
+        }
+        catch (Exception e){
+            if(number < -25){
+                System.out.println("Выполнилось условие в секции catch");
+                return;
+            }
+            System.out.println("Секция catch отработала до конца");
+        }
+        finally {
+            if(number > 0){
+                System.out.println("Выполнилось условие в секции finally");
+                return;
+            }
+            System.out.println("Секция finally отработала до конца");
+        }
+    }
+
+    public static int someMethod4() {
+
+        int number = (int) ((Math.random() - 0.5) * 100);
+
+        try{
+            if(number > 0){
+                return 1;
+            }
+            throw new FileNotFoundException("Исключение FileNotFoundException");
+        }
+        catch (Exception e){
+            if(number < -25){
+                return 2;
+            }
+            System.out.println("Секция catch отработала до конца");
+        }
+        finally {
+            if(number > 0){
+                return 3;
+            }
+            System.out.println("Секция finally отработала до конца");
+        }
+        return 0;
     }
 }
